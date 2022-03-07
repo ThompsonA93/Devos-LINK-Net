89a3c47720cd313b2ce45aad68ed55da2a23a527
ThompsonA93 <thauer@edu.aau.at>
2022-03-07 18:19:18 +0100

    Push little tests

--- static/js/web3connector.js
+++ static/js/web3connector.js
@@ -1,4 +1,19 @@
 const ethereumButton = document.getElementById(".enableEthereumButton");
   ethereumButton.addEventListener("click", () => {
-    ethereum.request({ method: 'eth_requestAccounts' });
+    ethereumButton.disabled = true;
+    ethereumButton.innerText = "Connecting ...";
+
+    const accounts = ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+
+    ethereumButton.disabled = false;
+    ethereumButton.innerText = account;
+  });
+
+/** 
+web3.eth.getAccounts(function(err, accounts){
+  if (err != null) console.error("An error occurred: "+err);
+  else if (accounts.length == 0) console.log("User is not logged in to MetaMask");
+  else console.log("User is logged in to MetaMask");
 });
+*/
